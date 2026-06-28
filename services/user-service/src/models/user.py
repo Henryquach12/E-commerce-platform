666c0edff70f3787d74409b45ef14e2e7ac5bcbe
Kien Hao Quach <a1942629@adelaide.edu.au>
2026-06-28 14:46:39 +0930

    Define users table structure.

---
+++ services/user-service/src/models/user.py
@@ -0,0 +1,24 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import String 
+from uuid import UUID, uuid4
+from datetime import datetime, timezone
+
+
+class Base(DeclarativeBase):
+    pass
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4)
+    user_name: Mapped[str] = mapped_column(String(50))
+    full_name: Mapped[str] = mapped_column(String(100), nullable=True) 
+    phone_number: Mapped[str] = mapped_column(String, unique=True)
+    address: Mapped[str] = mapped_column(String)
+    email: Mapped[str] = mapped_column(String, unique=True)
+    hashed_password: Mapped[str] = mapped_column(String)
+    created_at: Mapped[datetime] = mapped_column(default=lambda: datetime.now(timezone.utc))
+    is_active: Mapped[bool] = mapped_column(default=True)
+     
+    
+    
